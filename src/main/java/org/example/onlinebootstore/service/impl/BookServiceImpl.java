68d8e72625610bd68c9beaa2498b08116570976f
KLICKICE <ledmaskiight@gmail.com>
2025-07-22 21:06:44 +0300

    Fixed comments, and URL to PR

--- src/main/java/org/example/onlinebootstore/service/impl/BookServiceImpl.java
+++ src/main/java/org/example/onlinebootstore/service/impl/BookServiceImpl.java
@@ -1,17 +1,16 @@
 package org.example.onlinebootstore.service.impl;
 
-import lombok.*;
+import lombok.RequiredArgsConstructor;
 import org.example.onlinebootstore.model.Book;
 import org.example.onlinebootstore.repository.BookRepository;
 import org.example.onlinebootstore.service.BookService;
-import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
-    @Autowired
     private final BookRepository bookRepository;
 
     @Override
